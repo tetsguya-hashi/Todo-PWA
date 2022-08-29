78dedd184dc54b6398e24c247fd8d10151bee951
tetsuya-hashi <tty.84.m@gmail.com>
2022-08-29 18:35:33 +0900

    データを配列で取得した。

--- react-todo-pwa/src/components/Dashboard.jsx
+++ react-todo-pwa/src/components/Dashboard.jsx
@@ -8,11 +8,23 @@ import * as Api from '../service/api';
 const Dashboard = () => {
   const currentUser = useContext(AuthContext);
   const [inputName, setInputName] = useState('');
+  const [todos, setTodos] = useState([]);
 
   const post = () => {
     Api.addTodo(inputName, currentUser.currentUser.uid)
     setInputName('');
   }
+
+  useEffect(() => {
+    fecht();
+  }, [currentUser])
+  const fecht = async () => {
+    if (dig(currentUser, 'currentUser', 'uid')) {
+      const dataTodos = await Api.initGet(currentUser.currentUser.uid);
+      await setTodos(dataTodos);
+    }
+  }
+
   return (
     <div>
       {(dig(currentUser, 'currentUser')) ? (
--- react-todo-pwa/src/service/api.js
+++ react-todo-pwa/src/service/api.js
@@ -3,9 +3,28 @@ import { db } from './firebase';
 
 export const addTodo = (content, uid) => {
   db.collection('todo').add({
-    content: "test",
+    content: content,
     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
     isComplete: false,
-    uid: ""
+    uid: uid
   })
+}
+
+export const initGet = async (uid) => {
+  const todo = await db
+    .collection('todo')
+    .orderBy('createdAt', 'desc')
+    .where('uid', '==', uid);
+
+  return todo.get().then((snapShot) => {
+    let todos = [];
+    snapShot.forEach((doc) => {
+      todos.push({
+        id: doc.id,
+        content: doc.data().content,
+        isComplete: doc.data().isComplete
+      });
+    });
+    return todos;
+  });
 }
