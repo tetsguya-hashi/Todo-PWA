ed1b5391e9279aae143429c65833b5d3d3263737
tetsuya-hashi <tty.84.m@gmail.com>
2022-08-30 18:24:09 +0900

    PWAの設定をした。

---
+++ react-todo-pwa/src/components/ChecboxComponent.jsx
@@ -0,0 +1,22 @@
+import { Checkbox, ListItemIcon } from '@mui/material'
+import React, { useState } from 'react'
+import { toggleComplete } from '../service/api';
+
+const ChecboxComponent = ({ id, fetch, isComplete }) => {
+  const [complete, setComplete] = useState(false);
+
+
+  const checkHandle = async (id) => {
+    await toggleComplete(id);
+    fetch();
+  }
+  return (
+    <>
+      <ListItemIcon>
+        <Checkbox checked={isComplete} color="secondary" onChange={() => { checkHandle(id) }} />
+      </ListItemIcon>
+    </>
+  )
+}
+
+export default ChecboxComponent
