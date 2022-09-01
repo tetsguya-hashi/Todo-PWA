2ffdcbb322516c819643d5026a5a934d82805871
tetsuya-hashi <tty.84.m@gmail.com>
2022-09-01 11:17:24 +0900

    サインイン、サインアウトをリファクタリング

--- react-todo-pwa/src/service/firebase.js
+++ react-todo-pwa/src/service/firebase.js
@@ -1,21 +1,6 @@
-// v9 compat packages are API compatible with v8 code
-import firebase from 'firebase/compat/app';
 import { initializeApp } from "firebase/app" //v9
-import 'firebase/compat/auth';
-import { getAuth, GoogleAuthProvider } from "firebase/auth"; //v9
-import 'firebase/compat/firestore';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"; //v9
 import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore"; //9
-
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-});
 
 const firebaseApp = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -41,21 +26,30 @@ enableIndexedDbPersistence(db); //オフライン時の管理
 
 
 export const singInWithGoogle = () => {
-  firebase.auth().signInWithPopup(provider)
-    .then((res) => {
-      console.log(res.user);
-    })
-    .catch((error) => {
-      console.log(error.message);
-    })
+  // firebase.auth().signInWithPopup(provider).then((res) => {
+  //   console.log(res.user);
+  // }).catch((error) => {
+  //   console.log(error.message);
+  // });
+  const auth = getAuth();
+  signInWithPopup(auth, provider).then((res) => {
+    console.log(`ユーザー：${res.user}`);
+  }).catch((error) => {
+    console.log(error.message);
+  });
 }
 export const logOut = () => {
-  firebase.auth().signOut()
-    .then((res) => {
-      console.log(res.user);
-      document.location.reload();
-    })
-    .catch((error) => {
-      console.log(error.message);
-    })
+  // firebase.auth().signOut().then((res) => {
+  //   console.log(res.user);
+  //   document.location.reload();
+  // }).catch((error) => {
+  //   console.log(error.message);
+  // });
+  const auth = getAuth();
+  signOut(auth).then((res) => {
+    console.log(res.user);
+    document.location.reload();
+  }).catch((error) => {
+    console.log(error.message);
+  });
 }
